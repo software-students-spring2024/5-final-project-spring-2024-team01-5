b73bebe0e9121f4fe795a9961ded5673c24de83d
egnechng <egnechng@gmail.com>
2024-04-24 14:39:40 -0400

    Added page for adding grades

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 """Module providing framework for the web app."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from flask_cors import CORS
 from pymongo import MongoClient
 
 # DB Set up
+# TODO: Change these collection and db names if needed
+
 client = MongoClient("mongodb://localhost:27017/")
-db = client["audio-transcriptions"]
-collection = db["transcriptions"]
+db = client["GPACalculator"]
+collection = db["grades"]
 
 
 def create_app(test_config=None):
@@ -20,6 +22,7 @@ def create_app(test_config=None):
         Flask: The configured Flask application.
     """
     app = Flask(__name__)
+    app.secret_key = "secret_key"  # Set the secret key for flash messages
     CORS(app) # Enable CORS for all routes
     if test_config is not None:
         app.config.update(test_config)
@@ -30,7 +33,45 @@ def create_app(test_config=None):
         Route for the home page.
         """
         return render_template("index.html")
+    
 
+    @app.route("/add_grade", methods=["GET", "POST"])
 
+    def add_grade():
+        """
+        Route for the add course page.
+        """
+
+        if request.method == "POST":
+            # Handle the POST request
+            # Add the grade to the database or perform any other necessary actions
+            data = request.form
+            course_name = data["course_name"]
+            course_grade = data["course_grade"]
+            print(course_name, course_grade)
+            # Add a flash message
+            flash(f"Grade {course_grade} for {course_name} added successfully", "success")
+            return redirect(request.url)
+        else:
+            # Handle the GET request
+            return render_template("add.html")
+    
+    @app.route("/view_grades")
+    def view_grades():  
+        """
+        Route for the view grades page.
+        """
+
+        # Fetch the grades from the database
+        # when we add auth, find using user id
+        return render_template("view_grades.html")
+    
+    @app.route("/calculate")
+    def calculate():
+        """
+        Route for the calculate GPA page.
+        """
+        # calculate final grade of a class based on the weights of each secrtion
+        return render_template("calculate.html")
 
     return app
--- app/app.py
+++ app/app.py
@@ -1,37 +1,4 @@
-"""Module providing framework for the web app."""
-from flask import Flask, render_template
-from flask_cors import CORS
-from pymongo import MongoClient
-
-# DB Set up
-client = MongoClient("mongodb://mongodb:27017/")
-db = client["audio-transcriptions"]
-collection = db["transcriptions"]
-
-def create_app(test_config=None):
-    """
-    Create and configure the Flask application.
-
-    Args:
-        test_config (dict, optional): Configuration dictionary for testing purposes. Defaults to None.
-
-    Returns:
-        Flask: The configured Flask application.
-    """
-    app = Flask(__name__)
-    CORS(app) # Enable CORS for all routes
-    if test_config is not None:
-        app.config.update(test_config)
-
-    @app.route("/")
-    def home():
-        """
-        Route for the home page.
-        """
-        return render_template("index.html")
-
-    
-    return app
+from __init__ import create_app
 
 if __name__ == "__main__":
     app = create_app()
