fff221ce789c018556aadc217c7c641b43cb3b37
egnechng <egnechng@gmail.com>
2024-04-24 19:43:52 -0400

    Added functionality for calculating final grade

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@
 from flask import Flask, render_template, request, redirect, flash
 from flask_cors import CORS
 from pymongo import MongoClient
-
+from functions import calculate_final_exam_grade, calculate_GPA, calculate_course_grade
+import json
 # DB Set up
 # TODO: Change these collection and db names if needed
 
@@ -32,7 +33,7 @@ def create_app(test_config=None):
         """
         Route for the home page.
         """
-        return render_template("index.html")
+        return render_template("index.html", section="Home")
     
 
     @app.route("/add_grade", methods=["GET", "POST"])
@@ -46,9 +47,11 @@ def create_app(test_config=None):
             # Handle the POST request
             # Add the grade to the database or perform any other necessary actions
             data = request.form
+            # SAVE THIS DATA INTO MONGODB
             course_name = data["course_name"]
             course_grade = data["course_grade"]
-            print(course_name, course_grade)
+            course_credits = data["course_credits"]
+            # print(course_name, course_grade, course_credits)
             # Add a flash message
             flash(f"Grade {course_grade} for {course_name} added successfully", "success")
             return redirect(request.url)
@@ -62,16 +65,32 @@ def create_app(test_config=None):
         Route for the view grades page.
         """
 
-        # Fetch the grades from the database
-        # when we add auth, find using user id
-        return render_template("view_grades.html")
+        # Fetch the grades from MongoDB using find({user_id: user_id}), after we add auth
+        # before we add auth, we can test by fetching all data
+        return render_template("view_grades.html", section="View All Grades")
     
-    @app.route("/calculate")
+    @app.route("/calculate", methods=["GET", "POST"])
     def calculate():
         """
         Route for the calculate GPA page.
         """
-        # calculate final grade of a class based on the weights of each secrtion
-        return render_template("calculate.html")
+        if request.method == "POST":
+            data = request.data
+            print(data)
+            data = json.loads(data)
+
+            current_grade = float(data["current_grade"])
+            final_weight = float(data["final_weight"])
+            target_grade = float(data["target_grade"])
+            final_grade_needed = calculate_final_exam_grade(current_grade, final_weight, target_grade) # change this to calculate grade later
+             # Round to 2 decimal places
+            final_grade_needed = round(final_grade_needed, 2)
+            response = {
+                "final_grade": final_grade_needed
+            }
+            return response  # Return the calculated response
+        else:
+            # calculate final grade of a class based on the weights of each section
+            return render_template("calculate.html", section="Calculate Final Grade")
 
     return app
